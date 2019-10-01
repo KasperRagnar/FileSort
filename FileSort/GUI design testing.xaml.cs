d2dbd6cf3896ea57af50f8ea641c79e35b22ac59
Kasper <32301129+Moonshine42tech@users.noreply.github.com>
2019-10-01 20:17:53 +0200

    working on a new design

---
+++ FileSort/GUI design testing.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FileSort
+{
+    /// <summary>
+    /// Interaction logic for GUI_design_testing.xaml
+    /// </summary>
+    public partial class GUI_design_testing : Window
+    {
+        public GUI_design_testing()
+        {
+            InitializeComponent();
+        }
+    }
+}
